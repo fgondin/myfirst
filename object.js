12c6b361691a91b0a00a04ab057ba86ba33165dc
Frederico Gondin <108304623+fgondin@users.noreply.github.com>
2022-07-15 01:50:49 -0300

    Create object.js

---
+++ object.js
@@ -0,0 +1,16 @@
+class Candidato{
+    nome = "";
+    idade = "";
+    sexo = "";
+    cpf = "";
+    deficiencia = "";
+};
+
+var candidato = new Candidato()
+    candidato.nome = "Lucas";
+    candidato.idade = 15;
+    candidato.sexo = "m";
+    candidato.cpf = 12345678910;
+    candidato.deficiencia = "não";
+
+console.log (candidato)
